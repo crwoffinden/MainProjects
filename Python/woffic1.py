672b235fd6a510c73683ce05273da46be35c7419
crwoffinden <crwoffinden@gmail.com>
2024-01-20 21:55:11 -0700

    modified:   Python/woffic1.py 	new file:   Python/word-counts.json

--- Python/woffic1.py
+++ Python/woffic1.py
@@ -4,9 +4,42 @@ import argparse
 import os
 
 def main(inputString):
-    parser = argparse.ArgumentParser("Word Count")
-    parser.add_argument("--string", type=str, help="string to analyze")
-    args = parser.parse_args(inputString)
+    words = []
+    wordList = []
+    start = 0
+    for i in range(len(inputString)):
+        if string.whitespace.__contains__(inputString[i]):
+            word = inputString[start:i]
+            for x in string.punctuation:
+                word = word.replace(x, '')
+            word = word.lower() #FIXME may not work
+            if words.__contains__(word):
+                index = words.index(word)
+                wordList[index]["count"] += 1 #FIXME may not work
+            else:
+                words.append(word)
+                obj = {"word": word, "count": 1}
+                wordList.append(obj)
+            start = i + 1
+    word = inputString[start:]
+    for x in string.punctuation:
+        word = word.replace(x, '')
+    word = word.lower()  # FIXME may not work
+    if words.__contains__(word):
+        index = words.index(word)
+        wordList[index]["count"] += 1  # FIXME may not work
+    else:
+        words.append(word)
+        obj = {"word": word, "count": 1}
+        wordList.append(obj)
+    dictionary = {}
+    for i in range(len(words)):
+        dictionary[words[i]] = wordList[i]["count"]
+    cwd = os.getcwd()
+    fpath = os.path.join(cwd, "word-counts.json")
+    with open(fpath, "w") as jsonFile:
+        jsonObj = json.dumps(dictionary, indent=4)
+        jsonFile.write(jsonObj) #FIXME may not be what you think        jsonFile.write(jsonObj)
     # Remember to save the dictionary as a json file named "word-counts.json"
 
 
